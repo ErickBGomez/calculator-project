e4e3c4da2844ee25763836af70a3593a01419753
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-12-18 13:05:12 -0600

    Add numbers to currentData

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Hello world from JavaScript!");
+const DATA_MAX_LENGTH = 12;
+
+const currentData = document.querySelector("#current-data");
+
+const numberButtons = document.querySelectorAll(".number-button");
+const decimalPointButton = document.querySelector("#decimal-point");
+
+numberButtons.forEach(button => button.addEventListener("mousedown", updateCurrentData));
+
+function updateCurrentData(e) {
+    if (currentData.textContent.length < DATA_MAX_LENGTH) {
+        currentData.textContent += e.target.dataset.value;
+    }
+
+    // Prevent adding more than one decimal point
+    decimalPointButton.disabled = currentData.textContent.includes(".");
+}
