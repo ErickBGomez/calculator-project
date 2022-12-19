e4cf76422329f7807ea45c443e015327ef33e920
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-12-19 17:14:02 -0600

    Add backspace feature

--- js/main.js
+++ js/main.js
@@ -5,13 +5,27 @@ const currentData = document.querySelector("#current-data");
 const numberButtons = document.querySelectorAll(".number-button");
 const decimalPointButton = document.querySelector("#decimal-point");
 
-numberButtons.forEach(button => button.addEventListener("mousedown", updateCurrentData));
+const backspaceButton = document.querySelector("#backspace");
 
-function updateCurrentData(e) {
+numberButtons.forEach(button => button.addEventListener("mousedown", insertDigit));
+
+backspaceButton.addEventListener("mousedown", removeDigit)
+
+function insertDigit(e) {
     if (currentData.textContent.length < DATA_MAX_LENGTH) {
         currentData.textContent += e.target.dataset.value;
     }
 
+    checkDecimalPoint();
+}
+
+function removeDigit() {
+    currentData.textContent = currentData.textContent.slice(0, -1);
+
+    checkDecimalPoint();
+}
+
+function checkDecimalPoint() {
     // Prevent adding more than one decimal point
     decimalPointButton.disabled = currentData.textContent.includes(".");
 }
