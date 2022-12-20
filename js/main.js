6c71efd7d6b035c64c4d0f7f375c9243750f7769
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-12-19 18:35:41 -0600

    Add operators button feature and display previous data

--- js/main.js
+++ js/main.js
@@ -2,23 +2,40 @@
 const DATA_MAX_LENGTH = 12;
 
 let currentPrompt = "10";
+
 let firstOperand = 0;
 let secondOperand = 0;
 let selectedOperator = "";
 
 // DOM Queries
 const currentDataDisplay = document.querySelector("#current-data");
-
-const operatorButtons = document.querySelectorAll(".operator-button");
+const previousDataDisplay = document.querySelector("#previous-data");
 
 const numberButtons = document.querySelectorAll(".number-button");
 const decimalPointButton = document.querySelector("#decimal-point");
 
+const operatorButtons = document.querySelectorAll(".operator-button");
+
 const backspaceButton = document.querySelector("#backspace");
 const allClearButton = document.querySelector("#all-clear");
 
+function updateAllData() {
+    updateOperands();
+    updatePreviousData(firstOperand, selectedOperator);
+    
+    currentPrompt = "";
+}
+
 function updateCurrentData(newValue) {
     currentDataDisplay.textContent = (newValue || "0");
+}
+
+function updateOperands() {
+    firstOperand = +currentDataDisplay.textContent;
+}
+
+function updatePreviousData(firstValue, operator, secondValue = "") {
+    previousDataDisplay.textContent = `${firstValue} ${operator} ${secondValue}`;
 }
 
 function insertDigit(e) {
@@ -39,9 +56,15 @@ function removeDigit() {
     checkDecimalPoint();
 }
 
-function clearCurrentPrompt() {
+function clearAllData() {
     currentPrompt = "";
+    
+    firstOperand = 0;
+    secondOperand = 0;
+    selectedOperator = "";
+
     updateCurrentData(currentPrompt);
+    updatePreviousData("", "");
 }
 
 function checkDecimalPoint() {
@@ -49,10 +72,17 @@ function checkDecimalPoint() {
     decimalPointButton.disabled = currentPrompt.includes(".");
 }
 
+function selectOperator(e) {
+    selectedOperator = e.target.dataset.operator;
+
+    updateAllData();
+}
+
 // Events
 numberButtons.forEach(button => button.addEventListener("mousedown", insertDigit));
+operatorButtons.forEach(button => button.addEventListener("mousedown", selectOperator));
 backspaceButton.addEventListener("mousedown", removeDigit);
-allClearButton.addEventListener("mousedown", clearCurrentPrompt);
+allClearButton.addEventListener("mousedown", clearAllData);
 
 // First loading functions
 updateCurrentData(currentPrompt);
